1b6571068a0300bee2b180711d3fc7fe3a8bd483
inogiaco <inogiaco@gmail.com>
2025-07-17 23:30:01 +0900

    NewsList/index.tsx修正

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -41,7 +41,7 @@ export default function NewsList({ news }: Props){
                 <dt className={styles.title}>{article.title}</dt>
                 <dd className={styles.meta}>
                   <Category category={article.category} />
-                  <Date date={article.publishedAt} />
+                  <Date date={article.publishedAt ?? article.createdAt} />
                 </dd>  
               </dl>
             </Link>
